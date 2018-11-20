34801a8e61a8b01ce6f512e52ddd4ad3ea4aeac0
gregclinker <gregory.clinker@gmail.com>
2018-11-20 08:07:09 +0000

    first commit

---
+++ src/main/java/hello/HeaderFilter.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class HeaderFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		response.setHeader("GREG", "HELLO");
+		filterChain.doFilter(request, response);
+	}
+}
